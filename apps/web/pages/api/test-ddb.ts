b82ed12fd985967d403e62c67c94083f4ffa2bdd
Omar Aziz <github@omaraziz.dev>
2022-09-23 21:28:53 -0700

    fix database tsconfig

--- apps/web/pages/api/test-ddb.ts
+++ apps/web/pages/api/test-ddb.ts
@@ -1,11 +1,10 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { ddb } from 'database';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const users = await ddb.user.getAllUsers();
   const newAddress = users[users.length - 1].address + '1';
-  const newUser = await ddb.user.createUser(newAddress);
+  await ddb.user.createUser(newAddress);
   console.log({ users });
   res.status(200).json({ users });
 }
