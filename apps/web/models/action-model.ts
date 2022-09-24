8c93be694a27b1bb16bef0df82c7fd183a68407a
Thomas Han <thomashanthomashan@gmail.com>
2022-09-24 14:09:40 -0700

    Revert "modified action model for readability"
    This reverts commit 4a5c1a9713779e01287384acd3a97f2991c20351.

--- apps/web/models/action-model.ts
+++ apps/web/models/action-model.ts
@@ -3,50 +3,20 @@ export enum ActionType {
     ALERT_ACTION = 'ALERT_ACTION'
 }
 
-export enum MoneyStrategyType {
-    DEPOSIT_TO_AAVE = 'DEPOSIT_TO_AAVE',
-    SWAP_ON_UNISWAP = 'SWAP_ON_UNISWAP',
-}
-
-export enum SplitType {
-    EVENLY = 'EVENLY',
-    BY_PERCENTAGE = 'BY_PERCENTAGE'
-}
-
-// TBD, ignored for now
-export enum AlertType {
-    EMAIL = 'EMAIL',
-    TELEGRAM = 'TELEGRAM',
-    SLACK = 'SLACK',
-    SMS = 'SMS',
-    DISCORD = 'DISCORD',
-    PUSH_NOTIFICATION = 'PUSH_NOTIFICATION'
-}
-
 export interface ActionBaseModel {
     actionType: ActionType;
 }
 
 export interface MoneyActionModel extends ActionBaseModel {
-    splitType: SplitType;
+    splitType: 'EVENLY' | 'BY_PERCENTAGE';
     subActions: MoneyStrategy[];
 }
 
-export interface AlertActionModel extends ActionBaseModel {
-    subActions: AlertStrategy[];
+export enum MoneyStrategyType {
+    DEPOSIT_TO_AAVE = 'DEPOSIT_TO_AAVE',
+    SWAP_ON_UNISWAP = 'SWAP_ON_UNISWAP',
 }
 
-
-/** =================== sub strategies ===================
- * strategies here are sub components of actions
- *
- * For example, money action can have 3 different strategies
- * that distribute the money to 3 different places, in three
- * different ways.
- *
- * Alert action can have 3 different strategies that send
- * 3 different alerts to 3 different places.
- */
 export interface MoneyStrategy {
     actionType: MoneyStrategyType;
     originationAddress: string;
@@ -62,13 +32,13 @@ export interface MoneyStrategy {
     weight?: number;
 }
 
+export interface AlertActionModel extends ActionBaseModel {
+    subActions: AlertStrategy[];
+}
 
 export interface AlertStrategy {
-    actionType: AlertType;
+    actionType: 'EMAIL' | 'SMS' | 'PUSH_NOTIFICATION' | 'SLACK' | 'TELEGRAM' | 'DISCORD'; // etc
     message: string;
     title?: string;
     destinationPath: string;
-
-    // optional (not used)
-    // deliveryCadence?: 'DAILY' | 'IMMEDIATELY';
 }
