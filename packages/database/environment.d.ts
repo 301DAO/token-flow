c10997d70f619d15544e4a84e54b5eef76096a8a
Thomas Han <thomashanthomashan@gmail.com>
2022-09-25 11:46:51 -0700

    modifying aws related env vars

--- packages/database/environment.d.ts
+++ packages/database/environment.d.ts
@@ -1,8 +1,8 @@
 interface EnvironmentVariables {
   readonly NODE_ENV: 'development' | 'production';
-  readonly AWS_REGION: string;
-  readonly AWS_ACCESS_KEY_ID: string;
-  readonly AWS_SECRET_ACCESS_KEY: string;
+  readonly MY_AWS_REGION: string;
+  readonly MY_AWS_ACCESS_KEY_ID: string;
+  readonly MY_AWS_SECRET_ACCESS_KEY: string;
   readonly DYNAMODB_LOCAL_ENDPOINT: string;
 }
 
